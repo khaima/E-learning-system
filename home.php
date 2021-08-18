44bb185471449c66fc97b954fdee0d9c4120fb72
khaima <32354988+khaima@users.noreply.github.com>
2021-08-18 09:06:50 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,16 @@
+<!-- <h1><?php echo $title;?></h1> -->
+<div class="col-lg-12"> 
+<h3>Digital hub</h3>
+    <p> Digital hub is an adaptable set of open solutions specially developed to support learning for the Nothern communities without Internet access. Centered around an offline learning platform that runs on devices,<br>
+     the Digital hub Product Ecosystem includes a curricular tool, a library of open educational resources to support training and implementation in formal, informal, and non-formal learning environments.
+</p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4> 
+  <p>EXCELS: Excellence, Competence, and integration of content with ICT</p>
+
+  <h4>Mission</h4>
+  <p> A leading green hub in higher and continuing education committed to engage in quality instruction, development-oriented research, sustainable lucrative economic enterprise, and responsive extension and training services through relevant academic programs to empower a human resource that responds effectively to challenges in life and act as catalyst in the holistic development of a humane society.</p>
+</div>
+ 
+
